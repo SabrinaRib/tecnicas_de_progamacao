fe7c2eddc3e889135052ab36334b2a7497945627
Sabrina Ribeiro <sabrinarib924@gmail.com>
2025-11-27 22:24:13 -0300

    Delete Concessionaria directory

--- Concessionaria/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
-define('BASE', 'concessionaria');
-define('PORTA' , 3307);
-
-$conn = new MySQLi(HOST, USER, PASS, BASE, PORTA);
