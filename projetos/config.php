25e7f0d93f3d21e5152fa9ae0c68e4ffbbb496e2
Sabrina Ribeiro <sabrinarib924@gmail.com>
2025-11-27 21:22:07 -0300

    Delete projetos directory

--- projetos/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define('HOST', 'localhost');
-	define('USER', 'root');
-	define('PASS', '');
-	define('BASE', 'concessionariaberg');
-	define('PORT', 3307);
-	
-
-	$conn = new MySQLi(HOST,USER,PASS,BASE,PORT);
